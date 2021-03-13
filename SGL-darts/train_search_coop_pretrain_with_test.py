cacc5e875a6ae6804a3e02f17a58642f60a1c646
Balaji Shankar Balachandran <57128128+balajishankar04@users.noreply.github.com>
2021-03-13 00:06:44 -0800

    default value fix

--- SGL-darts/train_search_coop_pretrain_with_test.py
+++ SGL-darts/train_search_coop_pretrain_with_test.py
@@ -36,11 +36,11 @@ parser.add_argument('--weight_decay', type=float,
 parser.add_argument('--report_freq', type=float,
                     default=50, help='report frequency')
 parser.add_argument('--gpu', type=str, default='0', help='gpu device id')
-parser.add_argument('--epochs', type=int, default=45,
+parser.add_argument('--epochs', type=int, default=10,
                     help='num of training epochs')
 parser.add_argument('--init_channels', type=int,
-                    default=16, help='num of init channels')
-parser.add_argument('--layers', type=int, default=8,
+                    default=36, help='num of init channels')
+parser.add_argument('--layers', type=int, default=20,
                     help='total number of layers')
 parser.add_argument('--model_path', type=str,
                     default='saved_models', help='path to save the model')
@@ -49,7 +49,7 @@ parser.add_argument('--cutout', action='store_true',
 parser.add_argument('--cutout_length', type=int,
                     default=16, help='cutout length')
 parser.add_argument('--drop_path_prob', type=float,
-                    default=0.3, help='drop path probability')
+                    default=0.2, help='drop path probability')
 parser.add_argument('--save', type=str, default='EXP', help='experiment name')
 parser.add_argument('--seed', type=int, default=2, help='random seed')
 parser.add_argument('--grad_clip', type=float,
